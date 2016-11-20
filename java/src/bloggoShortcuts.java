ebac62bcd350bf302a853247632d5110954bea25
Baltzar Mattsson <baltzar@hotmail.se>
2016-11-20 11:46:52 +0100

    Update bloggoShortcuts.java

--- java/src/bloggoShortcuts.java
+++ java/src/bloggoShortcuts.java
@@ -1,9 +1,5 @@
 public class bloggoShortcuts {
 	
-	public static void main(String[] args) {
-		System.out.println(expand("Test -BBBB- *AAAAA*"));
-	}
-
 	public static String expand(String text) {
 		String retStr = "";
 		boolean closing = false;
@@ -12,13 +8,11 @@ public class bloggoShortcuts {
 			if (c != '-' && c != '*') {
 				retStr += c;
 			} else {
-				if (c == '-') {
+				if (c == '-')
 					retStr += closing ? "</i>" : "<i>";
-					closing = !closing;
-				} else if (c == '*') {
+				else if (c == '*')
 					retStr += closing ? "</b>" : "<b>";
-					closing = !closing;
-				}
+				closing = !closing;
 			}
 
 		}
